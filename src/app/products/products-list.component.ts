abdfb65871aec3b625f5cc5a31db050f87006730
SandeepGanesana <sandeep.ganesana@gmail.com>
2019-02-18 14:48:35 +0530

    services and Dependency Injection

--- src/app/products/products-list.component.ts
+++ src/app/products/products-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProducts } from './IProducts';
-import { timingSafeEqual } from 'crypto';
+import { productsService } from './products.service';
 @Component({
     selector: 'products-list',
     templateUrl: './products-list.component.html',
@@ -8,36 +8,15 @@ import { timingSafeEqual } from 'crypto';
 })
 export class ProductsListComponent implements OnInit {
     pageTitle: string = 'Products List';
-    products: IProducts[] = [
-        {
-            'productId': 1,
-            'productName': 'Leaf Rake',
-            'productCode': 'GDN-0011',
-            'releaseDate': 'March 19, 2016',
-            'description': 'Leaf rake with 48-inch wooden handle.',
-            'price': 19.95,
-            'starRating': 3.2,
-            'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-        },
-        {
-            'productId': 2,
-            'productName': 'Garden Cart',
-            'productCode': 'GDN-0023',
-            'releaseDate': 'March 18, 2016',
-            'description': '15 gallon capacity rolling garden cart',
-            'price': 32.99,
-            'starRating': 1.2,
-            'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-        }
-    ];
+    products: IProducts[] = [];
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     _productFilter: string;
     filteredProducts: IProducts[];
 
-    constructor () {
-        this.filteredProducts = this.products;
+    constructor (private productsService: productsService) {
+        
         this.productFilter = '';
     }
 
@@ -46,7 +25,8 @@ export class ProductsListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-
+        this.products = this.productsService.getProducts();
+        this.filteredProducts = this.products;
     }
 
     get productFilter(): string {
