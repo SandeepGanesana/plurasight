0ee73ef191753bbbbe07da182099ec2615ecd56b
SandeepGanesana <sandeep.ganesana@gmail.com>
2019-02-19 12:40:46 +0530

    'navigationGaurds';

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +26,9 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductsListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'products/:id', component: ProductDetailComponent,
+        canActivate: [ ProductDetailGuard ]
+      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { productsService } from '../products.service';
 import { IProducts } from '../IProducts';
 @Component({
@@ -7,14 +7,17 @@ import { IProducts } from '../IProducts';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail Page';
+  pageTitle: string = 'Product Detail :  ';
   product: IProducts[];
-  constructor(private route: ActivatedRoute, private _productsService: productsService) { }
+  constructor(private route: ActivatedRoute, private _productsService: productsService, private router: Router) {}
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `${id}`;
+    // this.pageTitle += `${id}`;
     this._productsService.getProductById(id).subscribe(res => this.product = res);
   }
 
+  OnBack(): void {
+    this.router.navigate(['/']);
+  }
 }
